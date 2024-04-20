70fd6c78673184460c1cfe9ed6a0199675a2ed12
TingYuHsieh <joanna88326@gmail.com>
2024-04-20 23:15:43 +0800

    [add]: create the basic layout

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,21 +3,31 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +44,64 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceLayout() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .safeDrawingPadding()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ArtworkPicture()
+        Spacer(modifier = Modifier.height(24.dp))
+        ArtworkInfo()
+        Spacer(modifier = Modifier.height(24.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .safeGesturesPadding()
+                .border(2.dp, Color.Blue),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            NavigationButtons()
+        }
+    }
+}
 
+@Composable
+fun ArtworkPicture(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.size(240.dp, 320.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        border = BorderStroke(2.dp, Color.Black)
+    ) {
+    }
+}
+
+@Composable
+fun ArtworkInfo(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .size(240.dp, 100.dp)
+            .background(MaterialTheme.colorScheme.surfaceVariant)
+    ) {
+
+    }
+}
+
+@Composable
+fun NavigationButtons(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Button(onClick = { /*TODO*/ }) {}
+        Button(onClick = { /*TODO*/ }) {
+        }
+    }
 }
 
 @Preview(showBackground = true)
